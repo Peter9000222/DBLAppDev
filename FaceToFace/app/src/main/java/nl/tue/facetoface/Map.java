fead7f7988b7d5ee403a8e285934b4f56c24f17c
Peter9000222 <p.g.r.m.j.gerards@student.tue.nl>
2017-03-15 15:03:01 +0100

    fixed action bar and nice open list button

--- FaceToFace/app/src/main/java/nl/tue/facetoface/Map.java
+++ FaceToFace/app/src/main/java/nl/tue/facetoface/Map.java
@@ -6,6 +6,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.location.Location;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,7 +15,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends FragmentActivity implements OnMapReadyCallback {
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
     //test voor iedereen
     //test 3
