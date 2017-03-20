42eb62c212b94f2396dad234761affa2307beb75
s125203 <kyanhtran@hotmail.com>
2017-03-21 00:27:23 +0100

    tabs in coordinatelayout icons in toolbar backbutton in inbox moet nog links

--- FaceToFace/app/src/main/java/nl/tue/facetoface/InboxActivity.java
+++ FaceToFace/app/src/main/java/nl/tue/facetoface/InboxActivity.java
@@ -1,13 +1,64 @@
 package nl.tue.facetoface;
 
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ActionMenuView;
 
 public class InboxActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ActionMenuView amvMenu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
+
+        //Action Bar with icon
+        toolbar = (Toolbar) findViewById(R.id.toolbar4);
+      /*  amvMenu = (ActionMenuView) toolbar.findViewById(R.id.amvMenu);
+        amvMenu.setOnMenuItemClickListener(new ActionMenuView.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem){
+                return onOptionsItemSelected(menuItem);
+            }
+                                           });*/
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Inbox");
+        }
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Received Requests"));
+        tabs.addTab(tabs.newTab().setText("Sent Requests"));
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_inbox, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.menuBackButton :
+                Intent intentMap = new Intent(this, Map.class);
+                startActivity(intentMap);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
