141ac507d5bfe7d4e0801feeed580e2d66dc7823
bernklh <bernklh@gmail.com>
2017-03-20 14:02:37 +0100

    Intent naar Topic Activity toegevoegd. Daarnaast wat UI dingen in de Topic Activity.

--- FaceToFace/app/src/main/java/nl/tue/facetoface/InboxActivity.java
+++ FaceToFace/app/src/main/java/nl/tue/facetoface/InboxActivity.java
@@ -3,7 +3,7 @@ package nl.tue.facetoface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class InboxActivity extends AppCompatActivity {
+public class InboxActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- FaceToFace/app/src/main/java/nl/tue/facetoface/TopicActivity.java
+++ FaceToFace/app/src/main/java/nl/tue/facetoface/TopicActivity.java
@@ -1,8 +1,17 @@
 package nl.tue.facetoface;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import static java.security.AccessController.getContext;
+import static nl.tue.facetoface.R.mipmap.ic_launcher;
 
 public class TopicActivity extends AppCompatActivity {
 
@@ -10,7 +19,28 @@ public class TopicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic);
-
-        Intent intent = getIntent();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_topic, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.save_button:
+                Intent intent = new Intent(this, Map.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
