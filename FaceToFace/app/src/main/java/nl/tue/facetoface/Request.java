b36f4d87d588838895ea5e4f1f454e7113cbd9fa
huytech <pianochap@live.com>
2017-03-22 13:11:47 +0100

    Added request model (unfinished).

---
+++ FaceToFace/app/src/main/java/nl/tue/facetoface/Request.java
@@ -0,0 +1,17 @@
+package nl.tue.facetoface;
+
+/**
+ * Created by piano on 22-Mar-17.
+ */
+
+public class Request {
+
+    public int senderID;
+
+    public int receiverId;
+
+    public String timeStamp;
+
+    public boolean status;
+
+}
