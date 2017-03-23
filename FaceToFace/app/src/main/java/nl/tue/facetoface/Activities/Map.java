de85e00e984ca1634f6d45434d8fec76d68a1cd7
s125203 <kyanhtran@hotmail.com>
2017-03-23 17:19:01 +0100

    Added Theme colors, tablayout color not correct  yet. Map centers on location each time activity is visited

--- FaceToFace/app/src/main/java/nl/tue/facetoface/Activities/Map.java
+++ FaceToFace/app/src/main/java/nl/tue/facetoface/Activities/Map.java
@@ -1,29 +1,53 @@
 package nl.tue.facetoface.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
 import nl.tue.facetoface.R;
 
-public class Map extends AppCompatActivity implements OnMapReadyCallback {
+public class Map extends AppCompatActivity implements OnMapReadyCallback, ConnectionCallbacks,
+        OnConnectionFailedListener {
+
+    private Toolbar tb;
 
     private GoogleMap mMap;
-    private Toolbar tb;
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+    protected Double mLatitude;
+    protected Double mLongitude;
+    protected Marker mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_map);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -39,15 +63,20 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
         }
         tb.inflateMenu(R.menu.menu);
 
-        /*Button goToTopic = (Button) findViewById(R.id.goToTopicButton);
-        goToTopic.setOnClickListener(new View.OnClickListener() {
+        //Move to location of the user when button clicked
+        FloatingActionButton centerMap = (FloatingActionButton) findViewById(R.id.fab);
+        centerMap.setImageResource(R.mipmap.ic_my_location_white_48dp);
+        centerMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), TopicActivity.class);
-                startActivity(intent);
+                LatLng locationUser = new LatLng(mLatitude, mLongitude);
+                mMap.addMarker(new MarkerOptions().position(locationUser).title("New Marker"));
+                mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(locationUser));
             }
-        });*/
+        });
 
+        buildGoogleApiClient();
     }
 
     @Override
@@ -71,21 +100,20 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
+       /* LatLng sydney = new LatLng(-33, 151);
+        mUser = mMap.addMarker(new MarkerOptions().position(sydney).title("You are here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        switch(id){
-            case R.id.inboxIcon :
+        switch (id) {
+            case R.id.inboxIcon:
                 Intent inboxIntent = new Intent(this, InboxActivity.class);
                 startActivity(inboxIntent);
                 break;
-            case R.id.topicIcon :
+            case R.id.topicIcon:
                 Intent topicIntent = new Intent(this, TopicActivity.class);
                 startActivity(topicIntent);
                 break;
@@ -94,4 +122,70 @@ public class Map extends AppCompatActivity implements OnMapReadyCallback {
         return super.onOptionsItemSelected(item);
     }
 
+    // Build Google API client
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    // Android activity life cycle method
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    // Android activity life cycle method
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    // Google API client connection callback
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitude = mLastLocation.getLatitude();
+            mLongitude = mLastLocation.getLongitude();
+            LatLng loc = new LatLng(mLatitude, mLongitude);
+
+            //Set location when the Map Activity is visited
+            mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+            mMap.addMarker(new MarkerOptions().position(loc).title("New Marker"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+        } else {
+            Toast.makeText(this, "No_location_detected", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Google API client connection callback
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        //Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+        Toast.makeText(this, "Connection failed", Toast.LENGTH_LONG).show();
+    }
+
+    // Google API client connection callback
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        //Log.i(TAG, "Connection suspended");
+        Toast.makeText(this, "Connection suspended", Toast.LENGTH_LONG).show();
+        mGoogleApiClient.connect();
+    }
 }
