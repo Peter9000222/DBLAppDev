3c706b05ff8f74b2c1c1d0c5a8a5c1f8038d58db
bernklh <bernklh@gmail.com>
2017-03-27 19:03:10 +0200

    Inbox Received en Sent hebben nu een recycleview. Elke recycleview item is clickable

--- FaceToFace/app/src/main/java/nl/tue/facetoface/Activities/InboxActivity.java
+++ FaceToFace/app/src/main/java/nl/tue/facetoface/Activities/InboxActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.GestureDetector;
@@ -16,23 +15,17 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ActionMenuView;
-import android.widget.Adapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import nl.tue.facetoface.Fragments.InboxReceivedListFragment;
 import nl.tue.facetoface.Fragments.InboxSentListFragment;
-import nl.tue.facetoface.InterestsAdapter;
 import nl.tue.facetoface.R;
 
 public class InboxActivity extends AppCompatActivity{
 
     private Toolbar tb;
-    private ActionMenuView amv;
-
-    ArrayList<String> inbox = new ArrayList<>();
-    RecyclerView InboxReceived_recyc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,7 +97,7 @@ public class InboxActivity extends AppCompatActivity{
             return super.onOptionsItemSelected(item);
         }
 
-    public static interface ClickListener{
+    public interface ClickListener{
         public void onClick(View view, int position);
     }
 
